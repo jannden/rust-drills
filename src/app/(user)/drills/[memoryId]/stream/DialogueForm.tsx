d779d43d517bdc66b74f08d6604df862f9074081
Jan Cibulka <78412925+jannden@users.noreply.github.com>
2024-03-21 09:41:48 +0200

    Form Submitting with Energy Info

--- src/app/(user)/drills/[memoryId]/stream/DialogueForm.tsx
+++ src/app/(user)/drills/[memoryId]/stream/DialogueForm.tsx
@@ -7,6 +7,7 @@ import Button, { ButtonType, ButtonVariant } from '@/components/user/Button'
 import { useCtrlEnterSubmit } from '@/lib/hooks/use-enter-submit'
 import { ChatRequestOptions } from 'ai'
 import CodeEditor from './CodeEditor'
+import Energy from '@/components/user/Energy'
 
 interface StoryFormProps {
   handleSubmit: (e: FormEvent<HTMLFormElement>, chatRequestOptions?: ChatRequestOptions | undefined) => void
@@ -17,6 +18,7 @@ interface StoryFormProps {
 
 export default function DialogueForm({ handleSubmit, isLoadingContent, input, handleInputChange }: StoryFormProps) {
   const { formRef, onKeyDown } = useCtrlEnterSubmit()
+
   return (
     <form onSubmit={handleSubmit} className="w-full" ref={formRef}>
       <CodeEditor
@@ -25,15 +27,22 @@ export default function DialogueForm({ handleSubmit, isLoadingContent, input, ha
         onKeyDown={onKeyDown}
         disabled={isLoadingContent}
       />
-      <Button
-        variant={ButtonVariant.Primary}
-        type={ButtonType.Submit}
-        disabled={isLoadingContent}
-        className="flex items-center gap-3"
-      >
-        {isLoadingContent ? <Loader2 className="size-5 animate-spin" /> : <Zap className="size-5" aria-hidden="true" />}
-        Submit
-      </Button>
+      <div className="flex justify-between">
+        <Button
+          variant={ButtonVariant.Primary}
+          type={ButtonType.Submit}
+          disabled={isLoadingContent}
+          className="flex items-center gap-3"
+        >
+          {isLoadingContent ? (
+            <Loader2 className="size-5 animate-spin" />
+          ) : (
+            <Zap className="size-5" aria-hidden="true" />
+          )}
+          Submit
+        </Button>
+        <Energy energyTimestamp={Date.now()} />
+      </div>
     </form>
   )
 }
