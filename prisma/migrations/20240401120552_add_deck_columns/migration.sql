1e6843278ce411ff352d04f2913d0b0a4f0c4b5c
Jan Cibulka <78412925+jannden@users.noreply.github.com>
2024-04-02 18:16:42 +0300

    Migration fix

--- prisma/migrations/20240401120552_add_deck_columns/migration.sql
+++ prisma/migrations/20240401120552_add_deck_columns/migration.sql
@@ -5,8 +5,40 @@ ALTER TABLE "Deck" ADD COLUMN "slug" TEXT;
 
 UPDATE "Deck" SET
 "canonicalUrl" = '',
-"slug" = ''
-WHERE "canonicalUrl" IS NULL;
+"slug" = '1'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '2'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '3'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '4'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '5'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '6'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '7'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '8'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
+UPDATE "Deck" SET
+"canonicalUrl" = '',
+"slug" = '9'
+WHERE "canonicalUrl" IS NULL AND "slug" IS NULL LIMIT 1;
 
 ALTER TABLE "Deck" ALTER COLUMN "canonicalUrl" SET NOT NULL;
 ALTER TABLE "Deck" ALTER COLUMN "slug" SET NOT NULL;
