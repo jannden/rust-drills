5cd3b0fe243906335739a52a8edbf3fde82aa27f
Jan Cibulka <jc@pntr.eu>
2024-05-20 18:03:23 +0300

    Seed script fix

--- prisma/seedScripts.ts
+++ prisma/seedScripts.ts
@@ -76,13 +76,3 @@ export async function reset() {
     console.log(`Created deck ${deck.title}.`)
   }
 }
-
-reset()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
