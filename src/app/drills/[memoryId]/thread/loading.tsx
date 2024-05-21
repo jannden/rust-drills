6876e50e0426abbc0502a105d81e4a8f910115b7
Jan Cibulka <jc@pntr.eu>
2024-05-21 11:43:49 +0300

    Removed Thread OpenAI Assistant integration

--- src/app/drills/[memoryId]/thread/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react'
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center pt-24">
-      <Loader2 className="size-10 animate-spin text-slate-200" />
-    </div>
-  )
-}
